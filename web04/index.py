8a115d0af7032f7b1b003d768c290f86b862d010
Teguh S <60564528+Teguhsut1207@users.noreply.github.com>
2024-01-29 17:34:36 +0700

    Add files via upload
    index.py updated with eWallet payment method

--- web04/index.py
+++ web04/index.py
@@ -6,9 +6,9 @@ import requests
 from flask import Flask, jsonify, request
 
 #!CONFIG
-Xendit_API_key = "your-Xendit-API-key"
+Xendit_API_key = "You-Xendit-API-key"
 api_version = "2022-07-31"
-
+BASE_URL="http://localhost:5000" #NOTE change it with your domain
 #====== CONFIG END ===============
 
 
@@ -51,6 +51,14 @@ def API_Send(url,method,header,body,print_input=False):
         response = requests.get(url, json=body, headers=header)
     return({'data':response.json(),'status_code':response.status_code})
 
+def set_header():
+    head={
+        'Authorization': f"Basic {basic_auth}",
+        'content-type': 'application/json',
+        'api-version':api_version
+    }
+    return (head)
+
 @app.route("/QRpayment", methods=["POST"])
 def createQR():
     url='https://api.xendit.co/qr_codes'
@@ -58,21 +66,18 @@ def createQR():
     data = request.get_json()
     total = int(data.get("total"))
     ref_id = data.get("reference_id")
+    curr=data.get("currency")
 
-    headers={
-        'Authorization': f"Basic {basic_auth}",
-        'api-version':api_version
-    }
     body={
         'reference_id':ref_id,
         'type':"DYNAMIC",
-        'currency':"IDR",
+        'currency':curr,
         "channel_code": "ID_LINKAJA",
         'expires_at':format_datetime(None,15*60), #for 15mins expiration
         'amount':list(total) if isinstance(total, set) else total 
     }
     
-    response = API_Send(url,'post', headers, body)
+    response = API_Send(url,'post', set_header(), body)
 
     if (not response['status_code']==201):
         return jsonify({"Error": response['data']}), response['status_code']
@@ -89,10 +94,6 @@ def createVA():
     bank = data.get("bank_code")
     name = data.get("name")
 
-    headers={
-        'Authorization': f"Basic {basic_auth}",
-        'api-version':api_version
-    }
     body={
         'external_id':ext_id,
         'bank_code':bank,
@@ -105,25 +106,87 @@ def createVA():
         'expiration_date': format_datetime(None,3600)    #for 1 hour expiration
     }
 
-    response = API_Send(url,'post', headers, body)
+    response = API_Send(url,'post', set_header(), body)
 
     if (not response['status_code']==200):
         return jsonify({"Error": response['data']}), response['status_code']
     else:
         return jsonify(response['data']), response['status_code']
 
+
+@app.route("/EWpayment", methods=["POST"])
+def createEW():
+    url='https://api.xendit.co/ewallets/charges'
+    
+    data = request.get_json()
+    total = int(data.get("total"))
+    ref_id = data.get("reference_id")
+    chann=data.get("channel_code")
+    if (chann not in ["ID_LINKAJA","ID_DANA","ID_OVO","ID_ASTRAPAY","ID_JENIUSPAY","ID_SHOPEEPAY","ID_SAKUKU"]):    #NOTE change the channel list if not Indonesian
+            return jsonify({"error":"Incorrect channel_code"}),400
+    phone=data.get("phone_number",None)
+    cashtag=data.get("cashtag",None)
+    
+    body={
+        
+            'reference_id':ref_id,
+            'currency':'IDR',
+            "channel_code": chann,
+            "checkout_method": "ONE_TIME_PAYMENT",
+            'expires_at':format_datetime(None,15*60), #for 15mins expiration
+            'amount':list(total) if isinstance(total, set) else total,
+        'channel_properties':{
+            "cashtag":cashtag,
+            "mobile_number":phone,
+            'success_redirect_url':BASE_URL+'/webhook/success',
+            'failure_redirect_url':BASE_URL+'/webhook/failed',
+        }
+    }
+
+    if (chann=="ID_JENIUSPAY"):
+        if (cashtag==None):
+            return jsonify({"error":"Cashtag needed for selected channel"}),400
+        else:
+            body.update(
+                {'channel_properties':{
+                    "cashtag":cashtag
+                }}
+            )
+    elif (chann=="ID_OVO"):
+        if (phone==None):
+            return jsonify({"error":"Phone number needed for selected channel"}),400
+        else:
+            if (phone==None or not isinstance(phone, str)):
+                return jsonify({"error":"Phone number needed in string format"}),400
+            body.update(
+                {'channel_properties':{
+                    "mobile_number":phone,
+                    'success_redirect_url':BASE_URL+'/webhook/success',
+                    'failure_redirect_url':BASE_URL+'/webhook/failed',
+                }}
+            )
+    else:body.update(
+                {'channel_properties':{
+                    'success_redirect_url':BASE_URL+'/webhook/success',
+                    'failure_redirect_url':BASE_URL+'/webhook/failed',
+                }}
+            )
+    
+    response = API_Send(url,'post', set_header(), body)
+
+    if (not response['status_code']==202):
+        return jsonify({"Error": response['data']}), response['status_code']
+    else:
+        return jsonify(response['data']), response['status_code']
+
+
 @app.route("/balance", methods=["POST"])
 def get_balance():
     url=['https://api.xendit.co/balance?account_type=CASH','https://api.xendit.co/balance?account_type=HOLDING','https://api.xendit.co/balance?account_type=TAX']
 
-    headers={
-        'Authorization': f"Basic {basic_auth}",
-        'api-version':api_version
-    }
     balance={}
     for n in url:
-        response = API_Send(n,'get', headers, {})
-        print(response)
+        response = API_Send(n,'get', set_header(), {})
         if (not response['status_code']==200):
             return jsonify({"Error": response['data']}), response['status_code']
         else:
@@ -131,11 +194,27 @@ def get_balance():
             balance[account_type] = response['data']['balance']
     return jsonify(balance), 200
 
+def process_payment(data, status):
+    # Common processing logic for both routes
+    # NOTE processing logic here
+    res=data
+    status="Payment "+status
+
+    return jsonify({"Status": status,"Output":res}), 200
+
 @app.route("/webhook", methods=["POST"])
-def handle_webhook():
-  data = request.get_json()
-  print(data)  #change this with further process after payment received
-  return jsonify({"success":"Payment Success"}), 200
+@app.route("/webhook/<status>", methods=["POST"])      #NOTE this line to handle callback from eWallet
+def handle_webhook(status=None):
+    data = request.get_json()
+    
+    if status is None:
+        if (data.get("event")=="payment.succeeded"): return process_payment(data, "success")
+        if (data.get("event")=="payment.failed"): return process_payment(data, "failed")
+        # Handle case when status is not provided
+        return jsonify({"Status": "Webhook Received"}), 200
+    else:
+        # Handle case when status is provided
+        return process_payment(data, status)
 
 
 if __name__ == "__main__":
